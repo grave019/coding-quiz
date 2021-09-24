9e5da3bbaf8f5446222bd506cca1d08ed679f016
Brent Graves <graves.brent@gmail.com>
2021-09-24 17:45:25 -0400

    changed brackets and parentheses to curly brackets.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,58 +1,58 @@
 //Declare variable Questions
 
 var questions = [
-    (
+    {
         title: "Which type of values return a true of false statement?",
-        choice: ["Boolean", "Empty Values", "Strings", "Numbers"],
+        choice: {"Boolean", "Empty Values", "Strings", "Numbers"},
         answer: "Boolean" 
-    ),
-    (
+    },
+    {
         title: "Where is the link for JavaScript inserted into a Web Page?",
-        choice: ["footer", "header", "top of the body", "bottom of the body"],
+        choice: {"footer", "header", "top of the body", "bottom of the body"},
         answer: "bottom of the body"
 
-    ),
+    },
     
-    (
+    {
         title: "Which of the following is an example of a comparison operator?",
-        choice: [ "*", "&&", "!=", "?:"],
+        choice: { "*", "&&", "!=", "?:"},
         answer: "!="
-    ),
-    (
+    },
+    {
         title: "What happens if the tested condition is false?",
-        choice: ["The code's execution will be stopped", "The code in the braces gets executed anyway",     "The code does nothing and moves to the next section", "The code executes the section"],
+        choice: {"The code's execution will be stopped", "The code in the braces gets executed anyway",     "The code does nothing and moves to the next section", "The code executes the section"},
         answer: "The code does nothing and moves to the next section"
-    ),
-    (   
+    },
+    {   
         title: "What is the result of trying to reference an array member which does not exist?",
-        choice: ["false", "0", "undefined", "null"],
+        choice: {"false", "0", "undefined", "null"},
         anwser: "undefined"
 
-    ),
-    (
+    },
+    {
         title: "An array has the length property, because it is:",
-        choice: ["an object", "a function", "a variable", "a element"],
+        choice: {"an object", "a function", "a variable", "a element"},
         anwser: "an object"
-    ),
-    (
+    },
+    {
         title: "The concat method takes two arrays and:",
-        choice: ["combines them into 1 new array", "compares their members", "outputs them to the screen",
-        "deletes the larger array"],
+        choice: {"combines them into 1 new array", "compares their members", "outputs them to the screen",
+        "deletes the larger array"},
         answer: "combines the into 1 new array"             
-    ),
-    (
+    },
+    {
         title: "In the Math Object, which of the following constants do not exist?",
-        choice: ["Math.PI", "Math.E", "Math.ABC", "Math.LN10"],
+        choice: {"Math.PI", "Math.E", "Math.ABC", "Math.LN10"},
         answer: "Math.ABC"
-    ),
-    (
+    },
+    {
         title: "In the Math Object, which of the following methods is used to calculate the square root?",
-        choice: ["round", "ceil", "sqrt", "root"],
+        choice: {"round", "ceil", "sqrt", "root"},
         answer: "sqrt"
-    ),
-    (
+    },
+    {
         title: "What does DOM stand for?",
-        choice: ["Document Object Model", "Document Orientation Model", "Definitive Orientation Model","Definitive Object Model"]
+        choice: {"Document Object Model", "Document Orientation Model", "Definitive Orientation Model","Definitive Object Model"},
         answer: "Document Object Model"
-    )
+    }
 ]
