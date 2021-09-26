07ed551a200385bb502f7608773aa9c1bddb1cde
Brent Graves <graves.brent@gmail.com>
2021-09-26 19:51:49 -0400

    added comments to html.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,60 @@
+//Array of questions
+var questions = [
+    {
+        title: "Which type of values return a true of false statement?",
+        choice: ["Boolean", "Empty Values", "Strings", "Numbers"],
+        answer: "Boolean" 
+    },
+    {
+        title: "Where is the link for JavaScript inserted into a Web Page?",
+        choice: {"footer", "header", "top of the body", "bottom of the body"},
+        answer: "bottom of the body"
+
+    },
+    
+    {
+        title: "Which of the following is an example of a comparison operator?",
+        choice: [ "*", "&&", "!=", "?:"],
+        answer: "!="
+    },
+    {
+        title: "What happens if the tested condition is false?",
+        choice: ["The code's execution will be stopped", "The code in the braces gets executed anyway",     "The code does nothing and moves to the next section", "The code executes the section"],
+        answer: "The code does nothing and moves to the next section"
+    },
+    {   
+        title: "What is the result of trying to reference an array member which does not exist?",
+        choice: ["false", "0", "undefined", "null"],
+        anwser: "undefined"
+
+    },
+    {
+        title: "An array has the length property, because it is:",
+        choice: ["an object", "a function", "a variable", "a element"],
+        anwser: "an object"
+    },
+    {
+        title: "The concat method takes two arrays and:",
+        choice: ["combines them into 1 new array", "compares their members", "outputs them to the screen",
+        "deletes the larger array"],
+        answer: "combines the into 1 new array"             
+    },
+    {
+        title: "In the Math Object, which of the following constants do not exist?",
+        choice: ["Math.PI", "Math.E", "Math.ABC", "Math.LN10"],
+        answer: "Math.ABC"
+    },
+    {
+        title: "In the Math Object, which of the following methods is used to calculate the square root?",
+        choice: ["round", "ceil", "sqrt", "root"],
+        answer: "sqrt"
+    },
+    {
+        title: "What does DOM stand for?",
+        choice: ["Document Object Model", "Document Orientation Model", "Definitive Orientation Model","Definitive Object Model"],
+        answer: "Document Object Model"
+    }
+];
 //declared variables
 var timerId;
 var currentQuestionIndex = 0;
@@ -9,8 +66,8 @@ var startBtn = document.querySelector("#start");
 var questionsElement = document.querySelector("#questions");
 var timerElement = document.querySelector("#choices");
 
-// seconds left is 15 seconds per question
-var secondsLeft = questions.length * 15;
+// seconds left is 15 seconds per question and 10 questions
+var secondsLeft = 150;
 // 
 var holdInterval = 0;
 // penalty for incorrect answer
@@ -28,7 +85,7 @@ timerElement.addEventListener("click", function(){
             if (secondsLeft <= 0) {
                 clearInterval(holdInterval);
                 allDone();
-                currentTime.textContent = "Time's Up!";
+                currentTime.textContent = "Sorry Time's Up!!";
             }
         }, 1000);
     }
@@ -52,12 +109,12 @@ function getcurrentQuestion(){
 
     }
 }
-}
+
 
 //start the quiz
 function startQuiz() {
     //hides hides start screen
-    let startscreen = document.querySelector("#start-screen");
+    var startscreen = document.querySelector("#start-screen");
     startscreen.setAttribute("class", "hide");
     
 
