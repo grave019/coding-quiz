ebf89d184428e0bd619fc4a919bd202869563a01
Brent Graves <graves.brent@gmail.com>
2021-09-26 14:09:37 -0400

    added seconds interval to questions.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,36 +1,49 @@
 //declared variables
-var time = questions.length * 15;
 var timerId;
 var currentQuestionIndex = 0;
-var score = 0
+var score = 0;
 
 //DOM elements
+var wrapper = document.querySelector("#wrapper");
 var startBtn = document.querySelector("#start");
 var questionsElement = document.querySelector("#questions");
 var timerElement = document.querySelector("#choices");
 
+// seconds left is 15 seconds per question
+var secondsLeft = questions.length * 15;
+// 
+var holdInterval = 0;
+// penalty for incorrect answer
+var penalty = 10;
+// creates new element
+var ulCreate = document.createElement("ul");
 
+// creates a countdown on the button shows on the display screen counting down
+timerElement.addEventListener("click", function(){
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function(){
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
 
-//start the quiz
-function startQuiz() {
-    //hides hides start screen
-    let startscreen = document.querySelector("#start-screen");
-    startscreen.setAttribute("class", "hide")
-
-    // shows questions
-    questionsElement.removeAttribute("class");
-    getcurrentQuestion();
-    
-}  
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's Up!";
+            }
+        }, 1000);
+    }
+    render(currentQuestionIndex)
+});
+// renders questions and choices to the page
 function getcurrentQuestion(){ 
-    let = currentQuestion = questions[currentQuestionIndex];
+    var = currentQuestion = questions[currentQuestionIndex];
     console.log(currentQuestion);
-    let titleElement = document.querySelector("#question-title");
+    var titleElement = document.querySelector("#question-title");
     titleElement.textContent = currentQuestion.title
     questionChoices.textContent = "";
 
     for (var i = 0; i < currentQuestion.questionChoices.length; i++) {
-        let choices = document.createElement("button");
+        var choices = document.createElement("button");
         choices.setAttribute("class", "choice");
         choices.setAttribute("value", currentQuestion.choice[i]);
 
@@ -39,5 +52,20 @@ function getcurrentQuestion(){
 
     }
 }
+}
+
+//start the quiz
+function startQuiz() {
+    //hides hides start screen
+    let startscreen = document.querySelector("#start-screen");
+    startscreen.setAttribute("class", "hide");
+    
+
+    // shows questions
+    questionsElement.removeAttribute("class");
+    getcurrentQuestion();
+    
+}  
+
 
 startBtn.addEventListener("click", startQuiz);
